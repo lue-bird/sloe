9e255098a31608583347d25af4f63344ba0e68f0
lue-bird <lue.the.bird@gmail.com>
2026-08-31 13:48:25 +0200

    switch zig hello world example to using arena allocator

--- example-hello-world-zig/index.zig
+++ example-hello-world-zig/index.zig
@@ -4,7 +4,7 @@ const sloe = @import("sloe.zig");
 pub fn main(init: std.process.Init) !void {
     const ResultOrigin = enum {};
     const result_origin = sloe.Origin(ResultOrigin, void){};
-    const greeting = try sloe.greet(@TypeOf(result_origin), init.gpa, .{
+    const greeting = try sloe.greet(@TypeOf(result_origin), init.arena.allocator(), .{
         .name = sloe.Str.fromComptime("world"),
         .buf = sloe.buf_empty(sloe.Char, ResultOrigin, void, result_origin),
     });
@@ -17,5 +17,5 @@ pub fn main(init: std.process.Init) !void {
         try writer.interface.print("{u}", .{greeting_char});
     }
     try writer.flush();
-    greeting.buf.rid(init.gpa);
+    greeting.buf.rid(init.arena.allocator());
 }
